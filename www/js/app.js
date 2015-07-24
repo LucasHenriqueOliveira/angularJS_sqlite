b1ea29be3403830b9e712191d3cea7f5eb38f992
Lucas <lucashen@gmail.com>
2015-07-23 23:28:17 -0300

    create routes

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,28 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+      .state('config', {
+        url: '/config',
+        templateUrl: 'templates/config.html',
+        controller: 'ConfigController'
+      })
+      .state('categories', {
+        url: '/categories',
+        templateUrl: 'templates/categories.html',
+        controller: 'CategoriesController'
+      })
+      .state('lists', {
+        url: '/lists/:categoryId',
+        templateUrl: 'templates/lists.html',
+        controller: 'ListsController'
+      })
+      .state('items', {
+        url: "/items/:listId",
+        templateUrl: "templates/items.html",
+        controller: "ItemsController"
+      });
+  $urlRouterProvider.otherwise('/config');
+});
